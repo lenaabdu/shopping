b499b85ce1a92ccaaabe8bfb38457600a63c5cdb
lenaabdu <lenaabdu23@gmail.com>
2020-08-18 11:01:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Basket from "./components/Basket";
+import store from "./store";
+import "./App.css";
+
+
+class App extends Component {
+ 
+  
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="container">
+          <h1>Shopping Cart </h1>
+          <hr />
+          <div className="row">
+            <div className="col-md-9">
+              <Filter />
+              <hr />
+              <Products />
+            </div>
+            <div className="col-md-3">
+              <Basket />
+        
+            </div>
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
