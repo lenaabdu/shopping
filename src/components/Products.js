e6ab5458a9031eafe562f3386c50d73a84399db6
lenaabdu <lenaabdu23@gmail.com>
2020-10-12 12:30:48 +0100

    commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,7 +12,7 @@ class Products extends Component {
       <div className="col-md-4" key={product.id}>
         <div className="thumbnail text-center">
           <a  href={`#${product.id}`} className="color"  onClick={(e) => this.props.addToCart(this.props.cartItems, product)}   >
-            <img src={`products/${product.sku}_2.jpg`} alt={product.title}  />
+            <img src= {`http://localhost:8000/products/${product.sku}_2.jpg`} alt={product.title}  />
             <p>{product.title}</p>
           </a>
           <b>{util.formatCurrency(product.price)}</b>
