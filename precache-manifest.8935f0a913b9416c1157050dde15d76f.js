8155dcc12216b09c40f44ac687ffa8e21d33794a
lenaabdu <lenaabdu23@gmail.com>
2020-10-12 14:40:51 +0100

    Updates

---
+++ precache-manifest.8935f0a913b9416c1157050dde15d76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4355108ff0d43d31f4aaa8d0c84f3343",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "bc574287409dfb22749e",
+    "url": "/shopping/static/css/main.15368248.chunk.css"
+  },
+  {
+    "revision": "03f9c3bc1fa5db6f6529",
+    "url": "/shopping/static/js/2.0d495b36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping/static/js/2.0d495b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc574287409dfb22749e",
+    "url": "/shopping/static/js/main.ba1de7dd.chunk.js"
+  },
+  {
+    "revision": "438f010c7b0342b5d4eb",
+    "url": "/shopping/static/js/runtime-main.d5c916ff.js"
+  }
+]);
